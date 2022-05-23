3b923a2a2a7a9e9b86f5fb986aea98d26ec00928
ChrisVaz1995 <christopher.vazquez2015@gmail.com>
2022-05-22 21:37:33 -0500

    created a img file and added images

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict'
+
+// const model = document.querySelector('.modal');
+// const btnOpenModel= document.querySelector('.show-model');
+// const btnCloseModel = document.querySelector('.close-model');
+
+// const openModal = function() {
+//     console.log('Button clicked');
+// }
+
